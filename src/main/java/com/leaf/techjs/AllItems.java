641c82e237d1f4fd277f2993d9ac7350e68084c8
lizhanyu-leaf <3854186771@qq.com>
2026-07-10 08:34:26 +0800

    R 1.0.3 正式版
    # 加入科技物品物品栏

--- src/main/java/com/leaf/techjs/AllItems.java
+++ src/main/java/com/leaf/techjs/AllItems.java
@@ -5,6 +5,9 @@ import com.leaf.techjs.context.items.TechUnlockerItem;
 import com.leaf.techjs.context.items.TechItemInfo;
 import com.leaf.techjs.context.items.TechItemsConfig;
 import com.leaf.techjs.kubejs.event.TechSystemRegisterEventJS;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.client.event.RegisterItemDecorationsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -21,13 +24,24 @@ import java.util.Map;
 
 @Mod.EventBusSubscriber(modid = TechSystemJS.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public final class AllItems {
+    public static final Map<TechInfo, RegistryObject<Item>> TECH_ITEMS = new HashMap<>();
+
     private static final DeferredRegister<Item> ITEMS
             = DeferredRegister.create(ForgeRegistries.ITEMS, TechSystemJS.MOD_ID);
 
-    private static boolean registered = false;
-    public static final Map<TechInfo, RegistryObject<Item>> TECH_ITEMS = new HashMap<>();
     public static final String TECH_ITEM_ICON_ID = "tech_item_icon";
     public static final RegistryObject<Item> TECH_ITEM_ICON = ITEMS.register(TECH_ITEM_ICON_ID, () -> new Item(new Item.Properties()));
+
+    private static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS
+            = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TechSystemJS.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> TECH_TAB = CREATIVE_MODE_TABS.register("tech_tab", () -> CreativeModeTab.builder()
+            .title(Component.translatable("itemGroup.techjs.tech_tab"))
+            .icon(() -> TECH_ITEM_ICON.get().getDefaultInstance())
+            .displayItems((params, output) -> TECH_ITEMS.values().forEach(item -> output.accept(item.get())))
+            .build());
+
+    private static boolean registered = false;
 
     public static void register(IEventBus bus) {
         if (registered) return;
@@ -42,6 +56,7 @@ public final class AllItems {
         }
 
         ITEMS.register(bus);
+        CREATIVE_MODE_TABS.register(bus);
     }
 
     @SubscribeEvent
